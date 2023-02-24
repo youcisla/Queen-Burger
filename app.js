100be2497331b536bcc39223d408f18c96f8ddae
JSGodX <nehmegabriel@outlook.com>
2023-02-24 20:41:23 +0100

    m

---
+++ app.js
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+function headerBuildHome(){
+    // variable difinitions
+    const element = document.querySelector("#header");
+
+    // functions and EventListeners
+    element.addEventListener('click',e =>{
+        element.classList.toggle("header_click");
+    });
+}
+
+headerBuildHome();
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+function bodyBuild(){
+    // variable difinitions
+    const element = document.querySelector(body);
+
+    // functions and EventListeners
+
+}
+
+bodyBuild()
