ac8eb18c34934d35b868487d8cf5182ad9823b1e
Titi-lg <121127293+Titi-lg@users.noreply.github.com>
2023-03-27 22:14:17 +0200

    Add files via upload
    Crud de la base de donne

---
+++ Base de donne/Personne.php
@@ -0,0 +1,33 @@
+<?php
+include 'index.php';
+function CreateTable(){
+    $sql1="CREATE TABLE IF NOT EXISTS Personne(
+        id INT NOT NULL AUTO_INCREMENT,
+        nom VARCHAR(100) NOT NULL UNIQUE,
+        prenom VARCHAR(100) NOT NULL ,
+        telephone VARCHAR(13) NOT NULL ,
+        CONSTRAINT pk_id PRIMARY KEY (id))";
+bdd()->query($sql);
+
+}
+function CreatePersonne($nom,$prenom,$telephone){
+    $sql = "INSERT INTO Personne(nom,prenom,telephone)Values('$nom','$prenom','$telephone')";
+    bdd()->query($sql);
+}
+function DeletePersonne($id){
+    $sql="DELETE FROM Personne WHERE id='$id'";
+    bdd()->query($sql);
+}
+function ReadPersonne($id){
+    $sql = "SELECT * from Personne where id = '$id' ";
+    $result=bdd()->query($sql);
+    $row = $result->fetch_assoc();
+    return $row;
+}
+function UpdatePersonne($id,$nom,$prenom,$telephone){
+    $sql = "UPDATE Personne SET
+    nom='$nom',prenom='$prenom',telephone='$telephone' where id = '$id'";
+    bdd()->query($sql);
+   
+}
+?>
---
+++ Base de donne/index.php
@@ -0,0 +1,12 @@
+<?php
+function bdd(){
+        $host="localhost";
+        $user = "root";
+        $pass = "";
+        $name="burgerqueen";
+        $bdd = new mysqli($host,$user,$pass,$name);
+        return $bdd;
+
+}
+
+?>
