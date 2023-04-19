732aa655b15b679e9a7a1e324df7b5ab0dd0b7ab
JSGodX <nehmegabriel@outlook.com>
2023-04-19 23:41:14 +0200

    Tout

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,29 +1,30 @@
 /////////////////////////////////////////////////////////////////////////////////////////////
 
-function headerBuildHome(){
-    // variable difinitions
-    const element = document.querySelector("#header");
-
-    // functions and EventListeners
-    element.addEventListener('click',e =>{
-        element.classList.toggle("header_click");
-    });
-    }
-
-headerBuildHome()
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-//test for header
 function test(){
-    var body = document.body,html = document.documentElement;
-    console.log( body.scrollHeight)
-    console.log(body.offsetHeight)
-    console.log(body.clientHeight)
+    var observer = new IntersectionObserver(
+        function(entries) {
+            const element = document.querySelector(".floating_header");
+
+            if(entries[0].isIntersecting === true){
+                console.log('Element has just become visible in screen');
+                element.classList.remove("floating_header_after_width");
+                element.classList.remove("floating_header_after");
+                element.removeEventListener('click',e =>{
+                    element.classList.toggle("floating_header_after_width");
+                });
+            }else{
+                element.classList.add("floating_header_after_width");
+                element.classList.add("floating_header_after");
+                // functions and EventListeners
+                element.addEventListener('click',e =>{
+                    element.classList.toggle("floating_header_after_width");
+                });
+            }
+        },
+        { threshold: [0] } // value 0 ~ 1
+    );
     
-    console.log(html.scrollHeight)
-    console.log(html.offsetHeight)
-    console.log(html.clientHeight)
+    observer.observe(document.querySelector("#header"));
 }
 test();
 /////////////////////////////////////////////////////////////////////////////////////////////
