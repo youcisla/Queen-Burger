02cb8a1dc1cc9e09eacfa867d9aac8c38c6a869e
Titi-lg <121127293+Titi-lg@users.noreply.github.com>
2023-04-24 19:58:53 +0200

    inscription v1

--- HTML/inscription.php
+++ HTML/inscription.php
@@ -7,6 +7,50 @@
     <title>Inscription</title>
 </head>
 <body>
-    
+    <div class="principal">
+        <div class="inscription">
+            <forum action="#">
+                </div>
+                <div class="nom">
+                <label for="Tnom">Nom</label>
+                <input type="nom" id="nom" name="nom" required>
+
+                </div>
+                <div class="prenom">
+                <label for="Tprenom">Prenom</label>
+                <input type="prenom" id="prenom" name="prenom" required>
+
+                </div>
+                <div class="telephone">
+                <label for="Tprenom">Prenom</label>
+                <input type="prenom" id="prenom" name="prenom" required>
+
+                </div>
+                <div class="login">
+                <label for="Tprenom">Prenom</label>
+                <input type="prenom" id="prenom" name="prenom" required>
+
+                </div>
+                <div class="mot_de_passe">
+                <label for="Tprenom">Prenom</label>
+                <input type="prenom" id="prenom" name="prenom" required>
+
+                </div>
+                <div class="confirmation">
+                <label for="Tprenom">Prenom</label>
+                <input type="prenom" id="prenom" name="prenom" required>
+
+                </div>
+                <div class="bouton_inscription">
+                <label for="Tprenom">Prenom</label>
+                <input type="prenom" id="prenom" name="prenom" required>
+
+                </div>
+            </forum>
+
+
+
+
+    </div>
 </body>
 </html>
