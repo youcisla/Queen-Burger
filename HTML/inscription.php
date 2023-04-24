59b5554497d73204a2b79f72f55724be25da4bcb
Titi-lg <121127293+Titi-lg@users.noreply.github.com>
2023-04-24 22:47:37 +0200

    inscription v1

--- HTML/inscription.php
+++ HTML/inscription.php
@@ -23,23 +23,23 @@
 
                 </div>
                 <div class="telephone">
-                <label for="Ttelephone">Prenom</label>
-                <input type="tel" id="telephone" name="telephone" placeholder="xxxxxxxxxx" pattern="[0-9]{9}" required>
+                <label for="Ttelephone">telephone</label>
+                <input type="tel" id="telephone" name="telephone" pattern="[0-9]{9}" required>
 
                 </div>
                 <div class="login">
-                <label for="Tlogin">Prenom</label>
-                <input type="login" id="prenom" name="prenom" required>
+                <label for="Tlogin">login</label>
+                <input type="login" id="login" name="login" required>
 
                 </div>
                 <div class="mot_de_passe">
-                <label for="Tprenom">Prenom</label>
-                <input type="prenom" id="prenom" name="prenom" required>
+                <label for="Tmot_de_passe">Mots de passes</label>
+                <input type="mot_de_passe" id="mot_de_passe" name="mot_de_passe" required>
 
                 </div>
                 <div class="confirmation">
-                <label for="Tprenom">Prenom</label>
-                <input type="prenom" id="prenom" name="prenom" required>
+                <label for="Tconfirmation">Confirmation</label>
+                <input type="confirmation" id="confirmation" name="confirmation" required>
 
                 </div>
                 <button class="bonton" onclick="">
