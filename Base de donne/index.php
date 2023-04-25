e2e13f647d2308cdd8f2733abca84ff47b9c241f
Titi-lg <121127293+Titi-lg@users.noreply.github.com>
2023-04-25 23:55:15 +0200

    tkt

--- Base de donne/index.php
+++ Base de donne/index.php
@@ -1,12 +1,6 @@
 <?php
-include_once "/Queen-Burger/bdd.php"
 function bdd(){
-        $host="localhost";
-        $user = "root";
-        $pass = "";
-        $name="burgerqueen";
-        $bdd = new mysqli($host,$user,$pass,$name);
-        return $bdd;
+        return mysqli_connect("localhost", "root", "", "base-de-donne");
 }
 
 ?>
