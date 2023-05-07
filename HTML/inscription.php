d978e807f25778a17c3d65564a7f6234b7019519
Titi-lg <121127293+Titi-lg@users.noreply.github.com>
2023-05-07 12:59:06 +0200

    inscription css

--- HTML/inscription.php
+++ HTML/inscription.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Queen-Burger\CSS\inscription.css"/>
     <title>Inscription</title>
 </head>
 <?php // potentille header 
 ?>
 <body>
     <div class="principal">
-        <div class="inscription">
+        <div class="burger-top">
           <h1>Inscription</h1>  
         </div>
             <form method="POST" action="/Queen-Burger/BaseDeDonne/validation.php">
