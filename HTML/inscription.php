f14c32309faada9deac1c5ed5077a8c368c458e2
Titi-lg <121127293+Titi-lg@users.noreply.github.com>
2023-05-07 13:02:41 +0200

    tkt

--- HTML/inscription.php
+++ HTML/inscription.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Queen-Burger\CSS\inscription.css"/>
+    <link rel="stylesheet" href="/Queen-Burger/CSS/inscription.css"/>
     <title>Inscription</title>
 </head>
 <?php // potentille header 
