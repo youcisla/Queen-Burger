34a347907d8df1e49091926a6dbba7cf0efcd322
Youcisla2.0 <60925626+youcisla@users.noreply.github.com>
2023-05-12 08:25:01 +0000

    connection

--- HTML/connexion.php
+++ HTML/connexion.php
@@ -1,3 +1,7 @@
+
+<?php
+include_once "../BaseDeDonne/indexx.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,14 +12,26 @@
     <title>Connexion</title>
 </head>
 <body>
-<form>
+<form action="action.php" method="post">
         <h2>Se connecter</h2>
         <label for="email">E-mail :</label>
         <input type="email" id="email" name="email" required>
         <label for="password">Mot de passe :</label>
         <input type="password" id="password" name="password" required>
         <input type="submit" value="Se connecter">
-    </form>
+</form>
+
+    <?php
+// Establish a database connection
+$c = bdd();
+
+// Query the database to get the employee data
+$sql = "SELECT * From personne where personne.login = {$_POST['email']} and personne.mot_de_passe = {$_POST['password']}";
+
+$result = $c->query($sql);
+
+$row = $result->fetch_assoc()
+?>
     
 </body>
 </html>
