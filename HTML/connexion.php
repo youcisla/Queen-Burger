7cbc28d9590e7573cf614940c27d7ffb21d50a2e
Youcisla2.0 <60925626+youcisla@users.noreply.github.com>
2023-05-13 13:21:47 +0200

    Update connexion.php

--- HTML/connexion.php
+++ HTML/connexion.php
@@ -1,9 +1,9 @@
-
 <?php
 include_once "../BaseDeDonne/indexx.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,27 +11,46 @@ include_once "../BaseDeDonne/indexx.php";
     <link rel="stylesheet" href="/Queen-Burger/CSS/connexion.css">
     <title>Connexion</title>
 </head>
+
 <body>
-<form action="action.php" method="post">
+    <!-- <form action="action.php" method="post"> -->
+    <form method="post">
         <h2>Se connecter</h2>
         <label for="email">E-mail :</label>
         <input type="email" id="email" name="email" required>
         <label for="password">Mot de passe :</label>
         <input type="password" id="password" name="password" required>
         <input type="submit" value="Se connecter">
-</form>
+    </form>
 
     <?php
-// Establish a database connection
-$c = bdd();
+include_once "../BaseDeDonne/indexx.php";
 
-// Query the database to get the employee data
-$sql = "SELECT * From personne where personne.login = {$_POST['email']} and personne.mot_de_passe = {$_POST['password']}";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Establish a database connection
+  $c = bdd();
 
-$result = $c->query($sql);
+  // Query the database to get the employee data
+  $sql = "SELECT * FROM `gerant` WHERE `gerant`.`login` = ? AND `gerant`.`mot_de_passe` = ?";
+  $stmt = $c->prepare($sql);
+  $stmt->bind_param('ss', $_POST['email'], $_POST['password']);
+  $stmt->execute();
 
-$row = $result->fetch_assoc()
+  $result = $stmt->get_result();
+
+  $row = $result->fetch_assoc();
+  if ($row) {
+    // Successful login, redirect to dashboard.php
+    header('Location: base.php');
+    exit();
+} else {
+    // Failed login, show error message
+    echo 'Invalid email or password';
+}
+}
 ?>
-    
+
+
 </body>
+
 </html>
