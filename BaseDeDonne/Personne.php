81deb4aa248e3a93be041f2905364ecac0b9aa2f
Youcisla2.0 <60925626+youcisla@users.noreply.github.com>
2023-05-15 14:01:13 +0200

    role

--- BaseDeDonne/Personne.php
+++ BaseDeDonne/Personne.php
@@ -48,28 +48,29 @@ function iDPersonne($nom,$prenom){
 function obtenirRolePersonne($id_personne) {
     $sql1 = "SELECT * FROM gerant WHERE information = {$id_personne}";
     $result1 = bdd()->query($sql1);
-    if($result1->rowCount() > 0) {
+    if ($result1->rowCount() > 0) {
         return 1;
     }
 
     $sql2 = "SELECT * FROM cuisinier WHERE information = {$id_personne}";
-    $result2 = bdd()->query($sql1);
-    if($result2->rowCount() > 0) {
+    $result2 = bdd()->query($sql2); // Corrected variable name from $sql1 to $sql2
+    if ($result2->rowCount() > 0) {
         return 2;
     }
 
     $sql3 = "SELECT * FROM serveur WHERE information = {$id_personne}";
-    $result3 = bdd()->query($sql1);
-    if($result3->rowCount() > 0) {
+    $result3 = bdd()->query($sql3); // Corrected variable name from $sql1 to $sql3
+    if ($result3->rowCount() > 0) {
         return 3;
     }
 
     $sql4 = "SELECT * FROM client WHERE information = {$id_personne}";
-    $result4 = bdd()->query($sql1);
-    if($result4->rowCount() > 0) {
+    $result4 = bdd()->query($sql4); // Corrected variable name from $sql1 to $sql4
+    if ($result4->rowCount() > 0) {
         return 4;
     }
 
     return 0;
 }
+
 ?>
