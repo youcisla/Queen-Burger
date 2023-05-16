8e6d6513d4dda8f4296c32f63819f44b40d0f7ed
TomyELPapel <thomasburfinp@gmail.com>
2023-05-16 21:46:47 +0200

    voila...

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+//magie
+header("Location: HTML/base.php");
+/*
 session_start();
 include_once "HTML/base.php";
 include_once 'BaseDeDonne/indexx.php';
+*/
 // include_once "HTML/calendar.php";
 ?>
