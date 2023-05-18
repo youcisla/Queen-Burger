2a731a26bddbd0a7be71b40cbc381c33d1e24b4a
Mathis <mathis.Friess@gmail.com>
2023-05-18 15:32:58 +0200

    mineur

--- HTML/gerant.php
+++ HTML/gerant.php
@@ -1,38 +1,12 @@
-<?php include_once '../BaseDeDonne/Personne.php';  ?>
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Queen Burger Head Master</title>
-    <link rel="stylesheet" href="style.css">
-</head>
 
-<body>
-    <?php
-        include_once "header.php";
-    ?>
+<?php 
+include_once '../BaseDeDonne/Personne.php';  
+include_once "../PHP/verificationConnexion.php";
+//redirectionConnexion([1,2], "base.php");
+?>
+<h1> Page gerant </h1>
 
-    <div class="allclasses">
-        <h1>Page Gerant</h1>
-        <div class='up'>
-            <div class='left'>
-                <button class="buttonSHAPE">
-                    <a href="/Queen-Burger/HTML/calendar.php">
-                        <i class="fas fa-calendar-alt"></i>
-                        Absences
-                    </a>
-                </button>
-            </div>
-
-            <div class='right'>
-                <form action="traitement-recherche.php" method="GET">
-                    <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                    <input type="text" name="q" placeholder="Rechercher...">
-                </form>
-            </div>
-        </div>
-</div>
 <?php 
 afficheRole("serveur");
 afficheRole("cuisinier");
@@ -41,11 +15,3 @@ afficheRole("gerant");
  ?>
 
 </table>
-</body>
-<footer>
-            <?php 
-            include_once 'footer.php';
-        ?>
-        </footer>
-
-        </html>
